a5825cf8406b0f9ca297231858ad12adbdadbd17
Rafał Giemza <rafalgiemza@gmail.com>
2023-10-15 14:21:29 +0200

    clean up

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-// components
-
-import CardLineChart from "@/components/Cards/CardLineChart.js";
-import CardBarChart from "@/components/Cards/CardBarChart.js";
 import CardPageVisits from "@/components/Cards/CardPageVisits.js";
 import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.js";
 
