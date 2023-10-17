5085ccd0db4ee13a54ff553ec52650dd9b719532
Rafał Giemza <rafalgiemza@gmail.com>
2023-10-17 10:50:39 +0200

    use HeaderStats on Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import HeaderStats from "@/components/HeaderStats/HeaderStats.js";
 import CardPageVisits from "@/components/Cards/CardPageVisits.js";
 import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.js";
 
 export default function DashboardPage() {
   return (
     <>
+      <HeaderStats />
       <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardPageVisits />
