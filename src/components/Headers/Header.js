77dc0d475e19593df19f17f17ebb3802c4c2fc6d
Rafał Giemza <rafalgiemza@gmail.com>
2023-10-17 10:48:27 +0200

    move HeaderStats to separate component

---
+++ src/components/Headers/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+
+import CardStats from "@/components/Cards/CardStats.js";
+
+export default function Header() {
+  return (
+    <>
+      {/* Header */}
+      <div className="relative bg-sky-600 md:pt-32 pb-32 pt-12" />
+    </>
+  );
+}
