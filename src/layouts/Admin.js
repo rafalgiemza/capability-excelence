11a41484cb9f081219b4cdbd52b1f9d8fafdbf28
Rafał Giemza <rafalgiemza@gmail.com>
2023-10-17 10:50:05 +0200

    use new Header in Admin layout

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import AdminNavbar from "@/components/Navbars/AdminNavbar.js";
 import Sidebar from "@/components/Sidebar/Sidebar.js";
-import HeaderStats from "@/components/Headers/HeaderStats.js";
+import Header from "@/components/Headers/Header.js";
 import FooterAdmin from "@/components/Footers/FooterAdmin.js";
 
 export default function Admin({ children }) {
@@ -14,7 +14,7 @@ export default function Admin({ children }) {
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
         {/* Header */}
-        <HeaderStats />
+        <Header />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           {children}
           <FooterAdmin />
